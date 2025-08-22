f03029328b00d2cf7dbc1ce5d68119d850220edc
Tommy Vo <contact@tommyvo.com>
2025-08-22 14:52:20 -0700

    Add Transaction model with description, amount (decimal), category, and metadata (jsonb)

---
+++ db/migrate/20250822215141_create_transactions.rb
@@ -0,0 +1,12 @@
+class CreateTransactions < ActiveRecord::Migration[8.0]
+  def change
+    create_table :transactions do |t|
+      t.string :description
+      t.decimal :amount
+      t.string :category
+      t.jsonb :metadata
+
+      t.timestamps
+    end
+  end
+end

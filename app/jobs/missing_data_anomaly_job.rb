7367f492d7228657880f4c0d194ff0025bd2b52a
Tommy Vo <contact@tommyvo.com>
2025-08-27 03:11:06 -0700

    Add background job to process MissingDataAnomaly
    This follows the pattern for `DuplicateTransactionAnomalyJob`

---
+++ app/jobs/missing_data_anomaly_job.rb
@@ -0,0 +1,14 @@
+class MissingDataAnomalyJob < ApplicationJob
+  queue_as :default
+
+  def perform(transaction_id)
+    transaction = Transaction.find_by(id: transaction_id)
+    return unless transaction
+
+    result = MissingDataAnomaly.call(transaction)
+    transaction.anomalies.where(anomaly_type: 'MissingData').destroy_all
+    if result
+      transaction.anomalies.create!(anomaly_type: result[:anomaly_type], reason: result[:reason])
+    end
+  end
+end
